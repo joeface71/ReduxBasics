5f5d6cbcb6670ba1654e2caa69a0f6e3897b79a6
joeface71 <joefacenda@yahoo.com>
2019-04-09 15:38:07 -0400

    FirstAssignment

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,6 +3,26 @@ const intitialState = {
 }
 
 const reducer = (state = intitialState, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            counter:  state.counter + 1
+        }
+    }
+    if (action.type === 'DECREMENT') {
+        return {
+            counter:  state.counter - 1
+        }
+    }
+    if (action.type === 'ADD') {
+        return {
+            counter:  state.counter + 5
+        }
+    }
+    if (action.type === 'SUBTRACT') {
+        return {
+            counter:  state.counter - 5
+        }
+    }
     return state;
 }
 
