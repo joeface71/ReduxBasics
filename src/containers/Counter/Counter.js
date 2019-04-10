13053653b16479452b4ee71a36d0c467451bf0d2
joeface71 <joefacenda@yahoo.com>
2019-04-10 19:06:11 -0400

    first commit

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -5,8 +5,6 @@ import CounterControl from "../../components/CounterControl/CounterControl";
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
 
 class Counter extends Component {
-  
-
   render() {
     return (
       <div>
@@ -19,14 +17,20 @@ class Counter extends Component {
           label="Decrement"
           clicked={this.props.onDecrementCounter}
         />
+        <CounterControl label="Add 10" clicked={this.props.onAddCounter} />
         <CounterControl
-          label="Add 5"
-          clicked={this.props.onAddCounter}
-        />
-        <CounterControl
-          label="Subtract 5"
+          label="Subtract 10"
           clicked={this.props.onSubtractCounter}
         />
+        <hr />
+        <button onClick={this.props.onStoreResult}>Store Result</button>
+        <ul>
+          {this.props.storedResults.map(strResult => (
+            <li onClick={() => this.props.onDeleteResult(strResult.id)} key={strResult.id}>
+              {strResult.value}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
@@ -34,17 +38,23 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
   return {
-      ctr: state.counter
+    ctr: state.counter,
+    storedResults: state.results
   };
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
-        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
-        onAddCounter: () => dispatch({type: 'ADD'}),
-        onSubtractCounter: () => dispatch({type: 'SUBTRACT'}),
-    }
-}
+  return {
+    onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
+    onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
+    onAddCounter: () => dispatch({ type: "ADD", value: 10 }),
+    onSubtractCounter: () => dispatch({ type: "SUBTRACT", value: 10 }),
+    onStoreResult: () => dispatch({ type: "STORE_RESULT" }),
+    onDeleteResult: (id) => dispatch({ type: "DELETE_RESULT", resultElId: id })
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
