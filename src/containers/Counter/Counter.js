1bf1a1de7ed613e2430170f25b8d125d3b707a60
joeface71 <joefacenda@yahoo.com>
2019-04-10 19:51:15 -0400

    combined reducers

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 import CounterControl from "../../components/CounterControl/CounterControl";
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
 
+import * as actionTypes from '../../store/actions'
+
 class Counter extends Component {
   render() {
     return (
@@ -23,7 +25,7 @@ class Counter extends Component {
           clicked={this.props.onSubtractCounter}
         />
         <hr />
-        <button onClick={this.props.onStoreResult}>Store Result</button>
+        <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
         <ul>
           {this.props.storedResults.map(strResult => (
             <li onClick={() => this.props.onDeleteResult(strResult.id)} key={strResult.id}>
@@ -38,19 +40,19 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
   return {
-    ctr: state.counter,
-    storedResults: state.results
+    ctr: state.ctr.counter,
+    storedResults: state.res.results
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
-    onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
-    onAddCounter: () => dispatch({ type: "ADD", value: 10 }),
-    onSubtractCounter: () => dispatch({ type: "SUBTRACT", value: 10 }),
-    onStoreResult: () => dispatch({ type: "STORE_RESULT" }),
-    onDeleteResult: (id) => dispatch({ type: "DELETE_RESULT", resultElId: id })
+    onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+    onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
+    onAddCounter: () => dispatch({ type: actionTypes.ADD, value: 10 }),
+    onSubtractCounter: () => dispatch({ type: actionTypes.SUBTRACT, value: 10 }),
+    onStoreResult: (result) => dispatch({ type: actionTypes.STORE_RESULT, result: result }),
+    onDeleteResult: (id) => dispatch({ type: actionTypes.DELETE_RESULT, resultElId: id })
   };
 };
 
---
+++ src/store/actions.js
@@ -0,0 +1,8 @@
+//use to help eliminate possibility of typos when using actions
+
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const ADD = 'ADD';
+export const SUBTRACT = 'SUBTRACT';
+export const STORE_RESULT = 'STORE_RESULT';
+export const DELETE_RESULT = 'DELETE_RESULT';
